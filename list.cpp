135d0076b09857ceb198f202d0a0c969d9e4b511
unknown <rgirong1@binghamton.edu>
2019-02-18 18:58:10 -0500

    changes to list

--- list.cpp
+++ list.cpp
@@ -5,20 +5,69 @@
 #include <cstdlib>
 
 List::List() {
-
 	Node *head = NULL;
 	Node *tail = NULL;
+}
 
+List::~List() {
+	delete head;
+	head = NULL;
 }
 
 void List::insert(int index, Planet *p) {
 	
+	Node temp = head;
+	Node newhead = head;
+	int temp_index = 0;
 	
+	if (size() <= index) {
+		while(temp->next != NULL) {
+			temp_index++;
+			temp = temp->next;
+			newhead = newhead->next;
+			
+			if (temp_index = index) {
+				newhead->p = p;
+				newhead = newhead->next;
+				break;
+			}
+		}
+	} else {
+		while(temp->next != NULL) {
+			temp_index++;
+			temp = temp->next;
+			newhead = newhead->next;
+			
+			if (temp->next = NULL) {
+				newhead = newhead->next;
+				newhead->p = p;
+				break;
+			}
+		}
+	}
 	
+	head = newhead;
 }
 
 Planet* List::read(int index) {
-
+	
+	if(head == NULL) {
+		return NULL;
+	}
+	
+	Node temp = head;
+	int temp_index = 0;
+	
+	while(temp->next != NULL) {
+		temp_index++;
+		temp = temp->next;
+		
+		if (temp_index = index) {
+			return temp->p;
+		}
+	}
+	
+	return NULL:
 }
 
 bool List::remove(int index) {
@@ -27,5 +76,13 @@ bool List::remove(int index) {
 
 unsigned List::size() {
 
-
+	Node temp = head;
+	unsigned temp_index = 0;
+	
+	while(temp->next != NULL) {
+		temp_index++;
+		temp = temp->next;		
+	}
+	
+	return temp_index;
 }
--- list.h
+++ list.h
@@ -1,14 +1,15 @@
 #ifndef lIST_H
 #define LIST_H
 
+class Node {
+public:
+	Planet *p;
+	Node *next;
+};
+
 class List {
 private:
 	Node *head, *tail;
-
-
-
-
-
 };
 
 #endif
