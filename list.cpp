8ca1621b6775dd7a14a4b4d715fdfd9db14d1787
Randolfo Giron <rgirong1@binghamton.edu>
2019-02-18 17:43:36 -0500

    List Files First Commit

---
+++ list.cpp
@@ -0,0 +1,31 @@
+#include "list.h"
+#include <stdlib.h>
+#include <iostream>
+#include <ctime> 
+#include <cstdlib>
+
+List::List() {
+
+	Node *head = NULL;
+	Node *tail = NULL;
+
+}
+
+void List::insert(int index, Planet *p) {
+	
+	
+	
+}
+
+Planet* List::read(int index) {
+
+}
+
+bool List::remove(int index) {
+
+}
+
+unsigned List::size() {
+
+
+}
---
+++ list.h
@@ -0,0 +1,14 @@
+#ifndef lIST_H
+#define LIST_H
+
+class List {
+private:
+	Node *head, *tail;
+
+
+
+
+
+};
+
+#endif
